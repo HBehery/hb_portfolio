16fcfb405af9c6cf08dbb3badc55c216bad71d82
HBehery <hamzahbehery@gmail.com>
2024-09-07 22:54:23 -0400

    V0.4.2: Projects page coming soon

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,46 +1,15 @@
-"use client";
+import React from "react";
+import NavBar from "../components/NavBar";
 
-import React, { useEffect } from "react";
-import Lenis from "lenis";
-import ProjectsPage from "../components/Projects";
-import { useScroll, useTransform } from "framer-motion";
-import { useRef } from "react";
-
-const ZoomProjects = () => {
-  useEffect(() => {
-    const lenis = new Lenis();
-
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-  });
-
-  const zoomContainer = useRef(null);
-
-  const { scrollYProgress } = useScroll({
-    target: zoomContainer,
-    offset: ["start start", "end end"],
-  });
-
-  const scale4 = useTransform(scrollYProgress, [0, 1], [0, 4]);
-
+const ProjectsPage = () => {
   return (
-    <div ref={zoomContainer} className="mt-[50vh] mb-[100vh]">
-      <div id="container" className="h-[200vh] relative">
-        <div
-          id="sticky"
-          className="sticky top-0 h-[100vh] bg-gray-900 overflow-hidden"
-        >
-          <div id="element" className="w-full h-full absolute top-0">
-            <ProjectsPage />
-          </div>
-        </div>
+    <div>
+      <NavBar />
+      <div className="dark:text-white text-gray-800 font-extrabold text-4xl sm:text-6xl flex w-[100vw] h-[70vh] items-center justify-center">
+        <h1>COMING SOON.</h1>
       </div>
     </div>
   );
 };
 
-export default ZoomProjects;
+export default ProjectsPage;
